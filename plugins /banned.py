6cae741e9910fd84f3cf35dbded482c7c66574d3
Hacker Jr  🧑‍💻 <70173542+HackerJr-TG@users.noreply.github.com>
2024-06-16 19:59:23 +0530

    banned.py🔹

--- plugins /banned.py
+++ plugins /banned.py
@@ -21,17 +21,17 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
+    await message.reply(f'Sorry Dude, You Are Banned To Use Me. \nBan Reason: {ban["ban_reason"]}')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('𝚂𝚞𝚙𝚙𝚘𝚛𝚝', url=f'https://t.me/KLAdmin1Bot')
+        InlineKeyboardButton('🧑‍🏫 Admin Support', url=f'https://t.me/KLAdmin1Bot')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
     k = await message.reply(
-        text=f"CHAT NOT ALLOWED 🐞\n\nMy admins has restricted me from working here ! If you want to know more about it contact support..\nReason : <code>{vazha['reason']}</code>.",
+        text=f"CHAT NOT ALLOWED 👀\n\nMy admins Has Restricted Me From Working Here ! If You Want To Know More About It Contact Support..\nReason : <code>{vazha['reason']}</code>.",
         reply_markup=reply_markup)
     try:
         await k.pin()
